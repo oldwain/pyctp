84e79c8c1d50cc4145e699f16ebe7be0a3c6c8b7
enceladus.smth <enceladus.smth@682a510a-1ea0-bb49-6162-20026ddc9db8>
2011-03-13 08:53:31 +0000

    config样本

---
+++ config-样本.py
@@ -0,0 +1,9 @@
+#-*- coding=utf-8 -*-
+
+
+SQ_USER = BaseObject(
+        port = u'tcp://asp-sim2-md1.financial-trading-platform.com:26213',
+        broker_id = u'2030',
+        investor_id = '*',
+        passwd = '*',
+    )
