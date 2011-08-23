ed265c04007bb9b012388911eebc0f46d6351d45
enceladus.smth <enceladus.smth@682a510a-1ea0-bb49-6162-20026ddc9db8>
2011-08-23 00:05:29 +0000

    初步的重要进展

--- mystrategy.py
+++ mystrategy.py
@@ -1,7 +1,9 @@
 # -*- coding: utf8 -*-
 
-from strategy import *
 import logging
+
+from base import *
+from strategy import *
 
 class day_long_break_30(LONG_BREAK):#每小时中间触发
     def check(self,data,ctick):
@@ -22,8 +24,7 @@ class day_long_stoper_35(DATR_LONG_STOPER):#触发后5分钟平仓
             return (True,ctick.low,False)
         return (False,0,False)
 
-
-class dl_break_nhh(LONG_BREAK): #nhh的实现
+class dl_break_nhh(LONG_BREAK): #nhhv的实现
     def check(self,data,ctick):
         pass
 
@@ -31,9 +32,34 @@ class dl_break_nhhv(LONG_BREAK): #nhhv的实现
     def check(self,data,ctick):
         pass
 
-class dl_break_mll2(SHORT_BREAK): #mll2的实现
+class dl_break_mll2(LONG_BREAK): #mll2的实现
+    def __init__(self,length=80,vbreak=10,vrange=270,vrange2=200,tlimit=1325):
+        LONG_BREAK.__init__(self)
+        self.length = length
+        self.vbreak = vbreak
+        self.vrange = vrange
+        self.vrange2 = vrange2
+        self.tlimit = tlimit
+
     def check(self,data,ctick):
-        pass
+        tlow = min(data.slow[-self.length:])[-1] + self.vbreak
+        ldmid = (data.d1[IHIGH][-1] + data.d1[IHIGH][-2])/2
+        vhigh = data.cur_day.vhigh
+        opend = data.cur_day.vopen
+        drange = (vhigh - data.cur_day.vlow)[-1]
+        if ctick.min1 < self.tlimit and drange < self.vrange:
+            slimit = vhigh - self.vrange
+        elif ctick.min1 > self.tlimit and drange < self.vrange:
+            slimit = vhigh - self.vrange2
+        else:
+            slimit = tlow
+        tlow = min(slimit,tlow,ldmid-60)
+        signal = data.slow[-1] > tlow and ctick.price < tlow
+        my_signal = (signal and data.sclose[-1] < tlow * 1.0015 and tlow < ldmid - 60 and data.ma13[-1] < data.ma30[-1]
+                        and vhigh - tlow < opend / 33
+                    )
+        return (True,0) if my_signal else (False,0)
+        
 
 class dl_break_mll2v(SHORT_BREAK): #mll2v的实现
     def check(self,data,ctick):
