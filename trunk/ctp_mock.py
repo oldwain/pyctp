e8cb2e82e5110656a5f9175512d6e7f9682ed9c3
enceladus.smth <enceladus.smth@682a510a-1ea0-bb49-6162-20026ddc9db8>
2011-09-22 00:07:55 +0000

    对nhh和mll2的小改动

--- trunk/ctp_mock.py
+++ trunk/ctp_mock.py
@@ -214,7 +214,7 @@ ticks = hreader.read_ticks(instrument,tday)    #不加载当日数据
 ctp_mock.run_ticks(ticks,myagent)
 
 ##推进
-tday = 20110826
+tday = 20110920
 myagent.scur_day = tday
 ticks = hreader.read_ticks(instrument,tday)    #不加载当日数据
 ctp_mock.run_ticks(ticks,myagent)
