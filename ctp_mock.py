5b87c92d7195a458f8974af02030e260fe7c3486
enceladus.smth <enceladus.smth@682a510a-1ea0-bb49-6162-20026ddc9db8>
2011-10-22 16:57:51 +0000

    步进

--- ctp_mock.py
+++ ctp_mock.py
@@ -198,8 +198,8 @@ import hreader
 
 ctp_mock.log_config()
 
-preday = 20111011
-tday = 20111012
+preday = 2011018
+tday = 20111019
 instrument = 'IF1111'
 myagent = ctp_mock.create_agent_with_mocktrader(instrument,-1)    #不需要tday的当日数据
 myagent.instruments[instrument].t2order = base.t2order_if
