11d1428c761c9f38ee65f2627848fe3f7ed6fb15
enceladus.smth <enceladus.smth@682a510a-1ea0-bb49-6162-20026ddc9db8>
2011-10-23 09:48:20 +0000

    小改动

--- trunk/ctp_mock.py
+++ trunk/ctp_mock.py
@@ -221,23 +221,33 @@ myagent.resume()
 ctp_mock.run_ticks(ticks,myagent)
 
 ##推进
+tday = 20110930
+myagent.day_switch(tday)
+ticks = hreader.read_ticks(instrument,tday)    #不加载当日数据
+ctp_mock.run_ticks(ticks,myagent)
+
+###############
+import base
+import ctp_mock
+import hreader
+
+ctp_mock.log_config()
+
+preday = 20110928
 tday = 20110929
-myagent.day_switch(tday)
+instrument = 'IF1110'
+myagent = ctp_mock.create_agent_with_mocktrader(instrument,-1,sname='strategy_trader.ini')    #不需要tday的当日数据
+myagent.instruments[instrument].t2order = base.t2order_if
+myagent.scur_day = preday
+#myagent.save_flag = True
+myagent.prepare_data_env()
+myagent.scur_day = tday
+#myagent.instruments[instrument].data.atrd1
 ticks = hreader.read_ticks(instrument,tday)    #不加载当日数据
+#ctp_mock.run_ticks(ticks,myagent)
+myagent.resume()
 ctp_mock.run_ticks(ticks,myagent)
 
-tday = 20110901
-myagent.day_switch(tday)
-ticks = hreader.read_ticks(instrument,tday)    #不加载当日数据
-ctp_mock.run_ticks(ticks,myagent)
-
-tday = 20110902
-myagent.day_switch(tday)
-ticks = hreader.read_ticks(instrument,tday)    #不加载当日数据
-ctp_mock.run_ticks(ticks,myagent)
-
-len(myagent.instruments['IF1109'].data.sclose)
-len(myagent.instruments['IF1109'].data.tr1)
 
 ################
 #第一次的ticks只到10:02
