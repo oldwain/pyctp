b0bdb677de3f43f5508bc37acec50d67228ded9d
enceladus.smth <enceladus.smth@682a510a-1ea0-bb49-6162-20026ddc9db8>
2011-12-20 14:13:12 +0000

    基本解决结算单问题

--- trunk/ctp_mock.py
+++ trunk/ctp_mock.py
@@ -222,7 +222,7 @@ myagent.resume()
 ctp_mock.run_ticks(ticks,myagent)
 
 ##推进
-tday = 20111214
+tday = 20111216
 myagent.day_switch(tday)
 ticks = hreader.read_ticks(instrument,tday)    #不加载当日数据
 ctp_mock.run_ticks(ticks,myagent)
@@ -235,8 +235,8 @@ import hreader
 
 ctp_mock.log_config()
 
-preday = 20111213
-tday = 20111214
+preday = 20111215
+tday = 20111216
 instrument = 'IF1201'
 myagent = ctp_mock.create_agent_with_mocktrader(instrument,-1,sname='strategy_trader.ini')    #不需要tday的当日数据
 myagent.instruments[instrument].t2order = base.t2order_if
