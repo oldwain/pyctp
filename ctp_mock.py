61c597ff5a652e8c2a742a63d7530e4f23d91ac4
wanfox@gmail.com <wanfox@gmail.com@682a510a-1ea0-bb49-6162-20026ddc9db8>
2012-02-19 14:03:20 +0000

    调整单手开仓所需资金，避免超价问题出现(使得违反了开仓不会由于资金不足而失败的假设, 因为资金失败时，撤单也不会成功)

--- ctp_mock.py
+++ ctp_mock.py
@@ -239,9 +239,9 @@ import hreader
 
 ctp_mock.log_config()
 
-preday = 20120208
-tday = 20120209
-instrument = 'IF1202'
+preday = 20120210
+tday = 20120213
+instrument = 'IF1203'
 myagent = ctp_mock.create_agent_with_mocktrader(instrument,-1,sname='strategy_trader.ini')    #不需要tday的当日数据
 myagent.instruments[instrument].t2order = base.t2order_if
 myagent.scur_day = preday
