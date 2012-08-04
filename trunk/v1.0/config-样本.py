b2808e2648804f2e6cbb294f00d4daee52122e70
wanfox@gmail.com <wanfox@gmail.com@682a510a-1ea0-bb49-6162-20026ddc9db8>
2012-08-04 06:11:36 +0000

    去掉v1.0,位置放错了

--- trunk/v1.0/config-样本.py
+++
@@ -1,9 +0,0 @@
-#-*- coding=utf-8 -*-
-
-
-SQ_USER = BaseObject(
-        port = u'tcp://asp-sim2-md1.financial-trading-platform.com:26213',
-        broker_id = u'2030',
-        investor_id = '*',
-        passwd = '*',
-    )
