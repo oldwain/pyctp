bec49a43c7aca1bed17b73589f7f48882b44a2dc
wanfox@gmail.com <wanfox@gmail.com@682a510a-1ea0-bb49-6162-20026ddc9db8>
2012-08-13 03:29:57 +0000

    添加indicator装饰器，调整指标编写和计算机制, 实现按需计算。 目前对存储的要求比较高。

--- trunk/base.py
+++ trunk/base.py
@@ -13,6 +13,12 @@ import sys
 import functools
 
 import logging
+import lib.decorator as decorator
+from inspect import (
+            getargspec,
+        )
+
+
 MY_FORMAT = '%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s'
 CONSOLE_FORMAT = '**%(message)s'
 
@@ -36,7 +42,15 @@ def add_log2console(level = logging.INFO):
     logging.getLogger('').addHandler(console)
     
 
-
+def date2week(iday):
+    #http://blog.csdn.net/hawkfeifei/article/details/4337181
+    year = iday/10000
+    month = iday/100%100
+    day = iday%100
+    if month <= 2:
+        month += 12
+        year -= 1
+    return (day+2*month+3*(month+1)/5+year+year/4-year/100+year/400)%7 + 1  #转化为1-7
 
 def inverse_direction(direction):
     return LONG if direction == SHORT else SHORT
@@ -83,6 +97,8 @@ class BaseObject(object):
     def __init__(self,**kwargs):
         self.__dict__.update(kwargs)
 
+
+    #has_attr/get_attr/set_attr没有必要, 系统函数hasattr/getattr/setattr已实现这些功能
     def has_attr(self,attr_name):
         return attr_name in self.__dict__
 
@@ -142,3 +158,161 @@ t2order_com = calc_t2order(859,1500)
 #模拟
 t2order_mock = calc_t2order(1900,2330,(2115,2115))
 t2order_mock2 = calc_t2order(1500,1930,(1715,1715))
+
+
+#快速键值
+'''
+    因为这里使用了id函数，所以必须妥善处理临时的输入序列参数
+        此时，某个序列对象被回收，然后会重新分配给另一个序列对象，
+        而这个对象又用于同一个被indicator修饰的函数,最终导致紊乱
+'''
+def quick_id(v):
+    '''
+        对基础数据类型，返回(v,None)
+        对对象类型，返回(id(v),v)
+    '''
+    t = type(v)
+    if t == list: #用到最多
+        return id(v),v
+    elif t  == int or t == float: #次多
+        return v,None
+    elif t in [long,bool,complex,str,unicode,xrange]:
+        return v,None
+    #其余都为对象，用id标识
+    return id(v),v
+
+def quick_ids(vs):
+    idv =  [quick_id(v) for v in vs]
+    return tuple((i for i,v in idv)),[v for i,v in idv if v!=None]
+
+
+def quick_objs(vs):
+    return [ v for v in vs if is_obj(v) ]
+
+import gc
+class ObjHolder(object):
+    '''
+        用于保持对象引用，避免释放、回收后被重新分配       
+    '''
+    def __init__(self):
+        self.holder = {}
+
+    def register_obj(self,obj):
+        if id(obj) not in self.holder:
+            self.holder[id(obj)] = obj
+        assert self.holder[id(obj)] is obj
+
+    def register_objs(self,objs):
+        for obj in objs:
+            self.register_obj(obj)
+
+    def reset(self):
+        self.holder.clear()
+        gc.collect()
+
+#用于持有住各indicator的输入对象，避免临时对象被回收后重新分配给用于同一indicator的序列对象，从而导致隐秘错误
+GLOBAL_HOLDER = ObjHolder()
+
+def source_len(vs):
+    '''
+        将根据source的长度判断是否需要更新
+    '''
+    for v in vs:
+        if type(v) == list:
+            return len(v)
+    return -1
+
+
+def _icache(func, *args, **kw):
+    '''
+        icache装饰器
+        根据参数中的第一个列表的长度判断是否需要重新计算
+        单独使用仅用于超级简单的情况
+        可以与@indicator一起使用，但提供的增益有限(因指标内部必须处理源序列增加的事宜),只减少了几个指标内部的判断语句
+        ####通常，任何情况下icache都是不必要的. icache引入的开销比不上它要节省的开销, 而且引入了额外的复杂性，更不宜于理解####
+
+        vojb用于固定住输入中用id标识的对象，防止在计算过程中被释放后重新分配给其它对象
+    '''
+    key = args + kw.iteritems() if kw else args
+    vkey,vobjs = quick_ids(key)
+    cache = func.cache # attributed added by memoize
+    slen = source_len(key)
+    if vkey not in cache or cache[vkey].slen < slen:
+        result = func(*args, **kw)
+        cache[vkey] = BaseObject(result = result,slen = slen,vobjs = vobjs)
+    elif slen > cache[vkey].slen:#须重新计算
+        cache[vkey].result = func(*args, **kw)
+        cache[vkey].slen = slen
+    return cache[vkey].result
+
+def icache(f):
+    f.cache = {}
+    return decorator.decorator(_icache,f)
+
+def _indicator(func, *args, **kw):
+    '''
+        indicator装饰器,用于常规indicator的实现
+        向原函数提供暂存对象, 要求原函数的最后一个位置参数必须是_ts
+        vojb用于固定住输入中用id标识的对象，防止在计算过程中被释放后重新分配给其它对象
+    '''
+    #print 'in _indicator'
+    vargs = list(args)
+    key = vargs + kw.values() if kw else vargs
+    #print vargs
+    vkey,vobjs = quick_ids(key)    
+    storage = func.storage
+    slen = source_len(key)
+    if vkey not in storage:
+        #storage[vkey] = BaseObject()
+        storage[vkey] = BaseObject()
+        GLOBAL_HOLDER.register_objs(vobjs)
+    #print vargs
+    #指标调用者直接指定_ts(用位置或命名参数)时，仍然将其替换为暂存者. 要求调用者不得指定这个参数，否则会导致莫名奇妙问题
+    #vargs[-1] = storage[vkey]
+    if vargs[-1] == None:  #最后一个位置参数为None,就认为是_ts；这里会略为出现紊乱，如出现刻意为None的其它参数
+        vargs[func.tpos] = storage[vkey]    #允许_ts不在最后位置，以支持可变参数*args
+    else:   #后面有可变位置参数, 此时,_ts被可变参数填充, 实际总长度比应当总长度少1，应将_ts插入到该位置
+        vargs = vargs[:func.tpos] + [storage[vkey]] + vargs[func.tpos:]
+        #print 'vargs=',vargs
+    return func(*vargs,**kw)
+
+def indicator(f):
+    f.storage = {}
+    aspecs = getargspec(f).args
+    f.tpos = aspecs.index('_ts')
+    assert f.tpos == len(aspecs)-1,'position of _ts is invalid'  #_ts必须是最后一个固定位置参数
+    return decorator.decorator(_indicator,f)
+
+##############
+# 一个举例
+#     可用的ma  
+##############
+#@icache
+@indicator
+def MA_EXAMPLE(src,mlen,_ts=None):
+    '''
+        所有指标都必须设定_ts这个参数,且默认值为None,装饰器将传入暂存对象
+        #_ts必须是最后一个固定位置参数
+
+        返回值:
+            移动平均序列
+            当序列中元素个数<mlen时，结果序列为到该元素为止的所有元素值的平均
+    '''
+    assert mlen>0,u'mlen should > 0'
+    if not hasattr(_ts,'sa'):
+        _ts.sa = [0]*mlen   #哨兵
+        _ts.ma = []
+
+    slen = len(_ts.ma)
+    ss = _ts.sa[-1]
+    for i in range(slen,len(src)):
+        ss += src[i]
+        _ts.sa.append(ss)
+        #print ss,_ts.sa[i-mlen]
+        #当累计个数<nlen时，求其平均值，而不是累计值/mlen
+        rlen = mlen if mlen < i+1 else i+1
+        _ts.ma.append((ss-_ts.sa[-rlen-1]+rlen/2)/rlen) 
+    #print _ts.sa
+    return _ts.ma
+
+
